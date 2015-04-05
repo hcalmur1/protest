1b43a9958ce507fc5843574b01b5d861b84772a3
hcalmur <hcalmur@gmail.com>
2015-04-04 18:31:21 -0600

    Bextlán Curso 23
    JavaScript: Detección de Plataformas y Navegadores

--- 05FundamentosJS/js/fundamentos8.js
+++ 05FundamentosJS/js/fundamentos8.js
@@ -4,8 +4,7 @@
  	Para saber más acerca de User Agent
 */
 var agente = navigator.userAgent.toLowerCase(),
-	plataforma = navigator.platform,
-	navegador = navigator.appCodeName,
+	plataforma,	navegador,
 	moviles = /iphone|ipod|ipad|android|blackberry|opera mini|iemobile|mobile/;
 
 //DECLARACIÓN DE FUNCIONES
@@ -39,11 +38,106 @@ function detectarDispositivo()
 	window.location.href = aDondeVoy;
 }
 
+function detectarPlataforma()
+{
+	if(agente.indexOf("win") > -1)
+	{
+		plataforma = (agente.search(moviles)>-1)?"Windows Phone":"Windows NT";
+	}
+	else if(agente.indexOf("mac") > -1)
+	{
+		plataforma = (agente.search(moviles)>-1)?"IOS":"Mac OS";
+	}
+	else if(agente.indexOf("linux") > -1)
+	{
+		plataforma = (agente.search(moviles)>-1)?"Android":"Linux";
+	}
+	else if(agente.indexOf("blackberry") > -1)
+	{
+		plataforma = "Blackberry";
+	}
+	else
+	{
+		plataforma = (agente.search(moviles)>-1)?"Plataforma móvil desconocida":"Plataforma de escritorio desconocida";
+	}
+
+	document.getElementById("plataforma").innerHTML = plataforma;
+}
+
+function detectarIE()
+{
+	var posicionIE, versionIE;
+	posicionIE = agente.indexOf("msie");
+	versionIE = agente.substring(posicionIE+5,posicionIE+8);
+
+	if(agente.indexOf("rv:11") > -1)
+	{
+		navegador = "IE 11";
+	}
+	else if(versionIE=="10.")
+	{
+		navegador = "IE 10";
+	}
+	else if(versionIE=="9.0")
+	{
+		navegador = "IE 9.0";
+	}
+	else if(versionIE=="8.0")
+	{
+		navegador = "IE 8.0";
+	}
+	else if(versionIE=="7.0")
+	{
+		navegador = "IE 7.0";
+	}
+	else if(versionIE=="6.0")
+	{
+		navegador = "IE 6.0";
+	}
+	else 
+	{
+		navegador = "Más viejo que IE 6.0, NO ME JODAS!!!";
+	}
+
+	return navegador;
+}
+
+function detectarNavegador()
+{
+	if( agente.indexOf("chrome") > -1 )
+	{
+		navegador = "Chrome";
+	}
+	else if( agente.indexOf("firefox") > -1 )
+	{
+		navegador = "Firefox";
+	}
+	else if( agente.indexOf("opera") > -1 )
+	{
+		navegador = "Opera";
+	}
+	else if( agente.indexOf("safari") > -1 )
+	{
+		navegador = "Safari";
+	}
+	else if( agente.indexOf("msie") > -1 || agente.indexOf("rv:11") >- 1 )
+	{
+		detectarIE();
+	}
+	else
+	{
+		navegador = "navegador desconocido";
+	}
+
+	document.getElementById("navegador").innerHTML = navegador;
+}
+
 function detectarUsuario()
 {
 	document.getElementById("user-agent").innerHTML = agente;
-	document.getElementById("plataforma").innerHTML = plataforma;
-	document.getElementById("navegador").innerHTML = navegador;
+	detectarPlataforma();
+	detectarNavegador();
 }
+
 //DECLARACION DE EVENTOS
 //window.addEventListener("load", cargaDocumento);
