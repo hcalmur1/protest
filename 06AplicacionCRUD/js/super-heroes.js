5b434901fdf137be43b977954aecd2436cb776d3
hcalzada <hcalmur@gmail.com>
2015-04-08 15:58:49 -0500

    Curso Terminado de la clase número 30 Introducción a AJAX

--- 06AplicacionCRUD/js/super-heroes.js
+++ 06AplicacionCRUD/js/super-heroes.js
@@ -10,7 +10,7 @@ var ajax = null,
 
 
 //Funciones
-/*   
+/*
 Función para detectar el tipo de objeto con el que se va a trabajar
 */
 function objetoAJAX()
@@ -28,21 +28,22 @@ function objetoAJAX()
 function enviarDatos()
 {
 	precarga.style.display= "block";
-	precarga.innerHTML = "<img src='img/loader.gif />";
+	precarga.innerHTML="<img src='img/loader.gif' />";
 	/*
 		http://es.wikipedia.org/wiki/Anexo:Códigos_de_estado_HTTP
 		http://librosweb.es/ajax/capitulo_7/metodos_y_propiedades_del_objeto_xmlhttprequest.html
 	 */
-
 	//Se verifica que este listo
 	if(ajax.readyState == READY_STATE_CHANGE)
 	{
 		if(ajax.status == OK)
 		{
+			console.log(ajax);
 			alert("Yes");
 		}
 		else
 		{
+			console.log(ajax);
 			alert("No");
 		}
 	}
@@ -54,7 +55,20 @@ function ejecutarAjax(datos)
 	ajax = objetoAJAX();
 	//Para detectar cambios de estado
 	ajax.onreadystatechange=enviarDatos;
+	/*
+		Un vez detectados los cambios se indica por que medio se enviará la información al servidor, en este caso POST y el archivo en BAckEnd que va a realizar el proceso
+	*/
 	ajax.open("POST","controlador.php");
+	/*
+		http://es.wikipedia.org/wiki/Multipurpose_Internet_Mail_Extensions#MIME-version
+		http://sites.utoronto.ca/webdocs/HTMLdocs/Book/Book-3ed/appb/mimetype.html
+
+	 */
+	/*
+		Configuración de la cabecera, en este caso se va a usar un formulario, por eso se usa este tipo de contenido
+	*/
+	ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	//Envío los datos
 	ajax.send(datos);
 
 }
